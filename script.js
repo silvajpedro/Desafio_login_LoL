ec6714955d61caa0b6eed347d79c328632df6aba
silvajpedro <belojpedro.31@gmail.com>
2022-07-08 08:29:39 -0300

    first part of responsive

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ firstInput.addEventListener('input', glow)
 secondInput.addEventListener('input', glow)
 
 function glow() {
-    if (firstInput.value.length >= 3 && secondInput.value.length <= 3 && secondInput.value.length >= 1) {
+    if (firstInput.value.length >= 3 && secondInput.value.length <= 4 && secondInput.value.length >= 1) {
         botao.classList.add('grown')
         passwordText.innerText = "Fraca"
         passwordBar.classList.add('passwordWeak')
-    } else if (firstInput.value.length >= 3 && secondInput.value.length <= 6 && secondInput.value.length >= 4) {
+    } else if (firstInput.value.length >= 3 && secondInput.value.length <= 7 && secondInput.value.length >= 5) {
         botao.classList.add('grown')
         passwordBar.classList.remove('passwordWeak')
         passwordBar.classList.add('passwordMedium')
         passwordText.innerText = "Média"
-    } else if ((firstInput.value.length >= 3 && secondInput.value.length <= 9 && secondInput.value.length >= 7)) {
+    } else if ((firstInput.value.length >= 3 && secondInput.value.length <= 10 && secondInput.value.length >= 8)) {
         botao.classList.add('grown')
         passwordBar.classList.remove('passwordMedium')
         passwordBar.classList.add('passwordStrong')
         passwordText.innerText = "Forte"
-    } else if (firstInput.value.length >= 3 && secondInput.value.length >= 9 && secondInput.value.length >= 7) {
+    } else if (firstInput.value.length >= 3 && secondInput.value.length >= 11 ) {
         passwordBar.classList.remove('passwordMedium')
         passwordBar.classList.add('passwordStrong')
         passwordText.innerText = "Forte"
