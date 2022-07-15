d87f1d7ce03f3ae760035083c9d9688f7c6fcbd2
silvajpedro <belojpedro.31@gmail.com>
2022-07-15 11:07:39 -0300

    ajuste no javascript

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ function glow() {
     if (firstInput.value.length >= 3 && secondInput.value.length <= 4 && secondInput.value.length >= 1) {
         botao.classList.add('grown')
         passwordText.innerText = "Fraca"
+        passwordBar.classList.remove('passwordMedium')
+        passwordBar.classList.remove('passwordStrong')
         passwordBar.classList.add('passwordWeak')
     } else if (firstInput.value.length >= 3 && secondInput.value.length <= 7 && secondInput.value.length >= 5) {
         botao.classList.add('grown')
         passwordBar.classList.remove('passwordWeak')
+        passwordBar.classList.remove('passwordStrong')
         passwordBar.classList.add('passwordMedium')
         passwordText.innerText = "Média"
     } else if ((firstInput.value.length >= 3 && secondInput.value.length <= 10 && secondInput.value.length >= 8)) {
         botao.classList.add('grown')
+        passwordBar.classList.remove('passwordMedium')
         passwordBar.classList.remove('passwordMedium')
         passwordBar.classList.add('passwordStrong')
         passwordText.innerText = "Forte"
